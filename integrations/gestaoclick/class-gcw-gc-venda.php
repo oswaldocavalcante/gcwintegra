b290bf98c93f3f2ff175aea340c9f200970f32eb
Oswaldo Cavalcante <jocmail@gmail.com>
2024-10-02 10:25:30 -0300

    Fix: Desconto aplicado à Venda não funciona.

--- integrations/gestaoclick/class-gcw-gc-venda.php
+++ integrations/gestaoclick/class-gcw-gc-venda.php
@@ -3,8 +3,8 @@
 require_once 'class-gcw-gc-api.php';
 require_once 'class-gcw-gc-cliente.php';
 
-class GCW_GC_Venda extends GCW_GC_Api {
-
+class GCW_GC_Venda extends GCW_GC_Api 
+{
     private $api_headers;
     private $api_endpoint;
 
@@ -12,7 +12,7 @@ class GCW_GC_Venda extends GCW_GC_Api {
     private $data;
     private $valor_frete;
     private $transportadora;
-    private $desconto_valor;
+    private $desconto_percentual;
     private $produtos = array();
 
     public function __construct($wc_order_id)
@@ -22,49 +22,63 @@ class GCW_GC_Venda extends GCW_GC_Api {
         $this->api_endpoint = parent::get_endpoint_sales();
 
         $order = wc_get_order($wc_order_id);
-        $this->desconto_valor   = $order->get_discount_total();
-        $this->data             = $order->get_date_created()->date('Y-m-d');
-        $this->valor_frete      = $order->get_shipping_total();
-        $this->transportadora   = $this->valor_frete == 0 ? '' : get_option('gcw-settings-export-trasportadora');
+        $this->desconto_percentual  = $order->get_total() > 0 ? ($order->get_discount_total() / $order->get_total()) * 100 : 0;
+        $this->data                 = $order->get_date_created()->date('Y-m-d');
+        $this->valor_frete          = $order->get_shipping_total();
+        $this->transportadora       = $this->valor_frete == 0 ? '' : get_option('gcw-settings-export-trasportadora');
 
-        $order_items = $order->get_items();
-        foreach ($order_items as $order_item) {
+        foreach ($order->get_items() as $order_item) 
+        {
             $order_item_data    = $order_item->get_data();
             $wc_product_id      = $order_item_data['product_id'];
             $wc_product         = wc_get_product($wc_product_id);
             $gc_product_id      = $wc_product->get_meta('gestaoclick_gc_product_id');
 
             $wc_variation_id = $order_item_data['variation_id'];
-            if ($wc_variation_id) {
+            if ($wc_variation_id) 
+            {
                 $wc_variation       = wc_get_product($wc_variation_id);
                 $gc_variation_id    = $wc_variation->get_meta('gestaoclick_gc_variation_id');
 
-                $this->produtos[] = array(
-                    'produto' => array(
-                        'produto_id'    => $gc_product_id,
-                        'variacao_id'   => $gc_variation_id,
-                        'quantidade'    => $order_item->get_quantity(),
-                        'valor_venda'   => $wc_product->get_price(),
+                $this->produtos[] = array
+                (
+                    'produto' => array
+                    (
+                        'produto_id'            => $gc_product_id,
+                        'variacao_id'           => $gc_variation_id,
+                        'quantidade'            => $order_item->get_quantity(),
+                        'valor_venda'           => $wc_product->get_price(),
+                        'tipo_desconto'         => '%',
+                        'desconto_porcentagem'  => $this->desconto_percentual,
                     )
                 );
-            } else {
-                $this->produtos[] = array(
-                    'produto' => array(
-                        'produto_id'    => $gc_product_id,
-                        'quantidade'    => $order_item->get_quantity(),
-                        'valor_venda'   => $wc_product->get_price(),
+            } 
+            else 
+            {
+                $this->produtos[] = array
+                (
+                    'produto' => array
+                    (
+                        'produto_id'            => $gc_product_id,
+                        'quantidade'            => $order_item->get_quantity(),
+                        'valor_venda'           => $wc_product->get_price(),
+                        'tipo_desconto'         => '%',
+                        'desconto_porcentagem'  => $this->desconto_percentual,
                     )
                 );
             }
         }
 
+        $wc_customer_id     = $order->get_customer_id();
+        $wc_customer        = new WC_Customer($wc_customer_id);
+        $this->cliente_id   = null;
+        
         // If a GestaoClick cliente_id exists, get it. Otherwise, export the new client and return his id from GestaoClick.
-        $wc_customer_id = $order->get_customer_id();
-        $wc_customer = new WC_Customer($wc_customer_id);
-        $this->cliente_id = null;
-        if ($wc_customer->get_meta('gestaoclick_gc_cliente_id')) {
+        if ($wc_customer->get_meta('gestaoclick_gc_cliente_id')){
             $this->cliente_id = $wc_customer->get_meta('gestaoclick_gc_cliente_id');
-        } else {
+        } 
+        else
+        {
             $gc_cliente = new GCW_GC_Cliente($wc_customer, 'order');
             $this->cliente_id = $gc_cliente->export();
             $wc_customer->add_meta_data('gestaoclick_gc_cliente_id', $this->cliente_id, true);
@@ -73,7 +87,8 @@ class GCW_GC_Venda extends GCW_GC_Api {
 
     public function export()
     {
-        $body = array(
+        $body = array
+        (
             'tipo'              => 'produto',
             'cliente_id'        => $this->cliente_id,
             'data'              => $this->data,
@@ -81,16 +96,19 @@ class GCW_GC_Venda extends GCW_GC_Api {
             'transportadora_id' => $this->transportadora,
             'valor_frete'       => $this->valor_frete,
             'nome_canal_venda'  => 'Internet',
-            'desconto_valor'    => $this->desconto_valor,
             'produtos'          => $this->produtos,
         );
 
-        $response = wp_remote_post( 
+        $response = wp_remote_post
+        ( 
             $this->api_endpoint, 
-            array_merge(
+            array_merge
+            (
                 $this->api_headers,
                 array( 'body' => wp_json_encode($body) ),
             ) 
         );
+
+        return $response;
     }
 }
